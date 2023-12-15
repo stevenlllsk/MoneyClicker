624385e3f9c2d1721511e3deb890fc9407f2d546
stevenlllsk <61805728+stevenlllsk@users.noreply.github.com>
2023-12-14 21:44:29 -0500

    changelog-handler.js

---
+++ changelog-handler.js
@@ -0,0 +1,9 @@
+function showChangelog() {
+    const changelogMenu = document.getElementById('changelog-menu');
+    changelogMenu.style.display = 'block';
+}
+
+function closeChangelog() {
+    const changelogMenu = document.getElementById('changelog-menu');
+    changelogMenu.style.display = 'none';
+}
