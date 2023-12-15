c214f1eccd6e7a339e59033844eb1f7ef613dc5a
stevenlllsk <61805728+stevenlllsk@users.noreply.github.com>
2023-12-14 22:17:23 -0500

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Your JavaScript code
+
 let money = 0;
 let upgradeLevel = 1;
 let upgradeCost = 2;
@@ -8,30 +9,80 @@ const moneyDisplay = document.getElementById('money');
 const upgradeBtn = document.getElementById('upgrade-btn');
 const upgradeLevelDisplay = document.getElementById('upgrade-level');
 
-clickerCircle.addEventListener('click', () => {
-    money += upgradeLevel; // Increase money based on the upgrade level
-    moneyDisplay.textContent = money;
+// Load saved data on page load
+window.addEventListener('load', () => {
+    loadGame();
 });
 
+// Save game data to localStorage
+function saveGame() {
+    const gameData = {
+        money,
+        upgradeLevel,
+        upgradeCost
+        // Add more properties if needed
+    };
+
+    localStorage.setItem('moneyClickerGameData', JSON.stringify(gameData));
+}
+
+// Load game data from localStorage
+function loadGame() {
+    const savedData = localStorage.getItem('moneyClickerGameData');
+
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+
+        money = parsedData.money;
+        upgradeLevel = parsedData.upgradeLevel;
+        upgradeCost = parsedData.upgradeCost;
+
+        // Update displays with loaded data
+        moneyDisplay.textContent = money;
+        upgradeLevelDisplay.textContent = upgradeLevel;
+        updateUpgradeCostDisplay();
+    }
+}
+
+// Add a function to reset the game
+function resetGame() {
+    localStorage.removeItem('moneyClickerGameData');
+    money = 0;
+    upgradeLevel = 1;
+    upgradeCost = 2;
+    moneyDisplay.textContent = money;
+    upgradeLevelDisplay.textContent = upgradeLevel;
+    updateUpgradeCostDisplay();
+}
+
+// Clicker circle event listener
+clickerCircle.addEventListener('click', () => {
+    money += upgradeLevel;
+    moneyDisplay.textContent = money;
+    saveGame(); // Save game data after each click
+});
+
+// Upgrade button event listener
 upgradeBtn.addEventListener('click', () => {
     if (money >= upgradeCost) {
         money -= upgradeCost;
         upgradeLevel += 1;
-        upgradeCost = Math.ceil(upgradeCost * 1.1); // Increase cost by 10% (adjust as needed)
-
-        // Update displays
+        upgradeCost = Math.ceil(upgradeCost * 1.1);
         moneyDisplay.textContent = money;
         upgradeLevelDisplay.textContent = upgradeLevel;
         updateUpgradeCostDisplay();
+        saveGame(); // Save game data after upgrading
     } else {
         alert("Not enough money to upgrade!");
     }
 });
 
+// Function to update upgrade cost display
 function updateUpgradeCostDisplay() {
     upgradeBtn.textContent = `Upgrade Clicker ($${upgradeCost})`;
 }
 
+// Function to create snowflakes
 function createSnowflakes() {
     const snowflakesContainer = document.querySelector('.snowflakes');
 
